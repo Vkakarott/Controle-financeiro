82e6c84dba99d2701dc25a69ad7f3ea604b42ed0
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2024-07-19 23:35:36 -0300

    ...

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-export default function Button({ children, onClick }) {
+export default function Button({ children, onClick, className = "", type = "button", ariaLabel = "", ...props }) {
     return (
-        <button onClick={onClick} className="rounded-full">
+        <button
+            onClick={onClick}
+            className={`rounded-full ${className}`}
+            type={type}
+            aria-label={ariaLabel}
+            {...props}
+        >
             {children}
         </button>
-    )
+    );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,22 +3,22 @@
 import React from "react";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Button from "./Button";
-import { signOut } from "@/auth";
+import { signOut } from "next-auth/react";
 
 export default function NavBar() {
     return (
         <nav className="flex items-center justify-between flex-col h-full px-5 py-8 text-2xl gap-7">
             <div className="flex flex-col justify-between h-full bg-[var(--picton-blue)] px-3 py-6 rounded-full gap-12">
                 <div className="flex flex-col gap-3">
-                    <Button><i className="bi bi-house"></i></Button>
-                    <Button><i className="bi bi-bar-chart"></i></Button>
-                    <Button><i className="bi bi-calendar3"></i></Button>
+                    <Button ariaLabel="Home"><i className="bi bi-house"></i></Button>
+                    <Button ariaLabel="Statistic"><i className="bi bi-bar-chart"></i></Button>
+                    <Button ariaLabel="Calendar"><i className="bi bi-calendar3"></i></Button>
                 </div>
                 <div className="flex flex-col gap-3 mt-16">
-                    <Button><i className="bi bi-gear"></i></Button>
+                    <Button ariaLabel="config"><i className="bi bi-gear"></i></Button>
                 </div>
             </div> 
-            <Button onClick={() => signOut()}><i className="bi bi-box-arrow-left"></i></Button>
+            <Button ariaLabel="SignOut" onClick={() => signOut()}><i className="bi bi-box-arrow-left"></i></Button>
         </nav>
     )
 }
