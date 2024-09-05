16e4a4baaf55825cd4d219229b059bd5e8fc4df1
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2024-09-05 03:35:17 -0300

    feat: re-init

--- src/components/Item.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Item() {
-    return (
-        <div className="flex justify-center items-center rounded-3xl col-start-5 col-end-7 row-start-3 row-end-5 bg-[var(--cards)] border-[var(--zinc)] border shadow-md">
-            card
-        </div>
-    );
-}
