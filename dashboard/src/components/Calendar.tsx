885ca772313a7127e5bee3c7ae11997339fe9ae8
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2024-09-11 14:26:12 -0300

    feat: refactor calendar

--- dashboard/src/components/Calendar.tsx
+++ dashboard/src/components/Calendar.tsx
@@ -2,23 +2,25 @@ import React from "react";
 
 export default function Calendar() {
     const date = new Date();
-    const month_name = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"][date.getMonth()];
+    const month_name = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][date.getMonth()];
     const day = date.getDate();
     const first_day = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
     const last_day = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
     return (
         <div className="flex flex-col w-[95%] px-2 pb-4 mb-3 rounded-2xl bg-[var(--calendar)] items-center">
-            <h1 className="font-semibold p-1">{month_name}</h1>
+            <h1 className="font-semibold p-1">
+                {month_name}
+            </h1>
             <div>
                 <ul className="flex text-sm font-medium items-center justify-between text-[var(--text)]">
-                    <li>Dom</li>
-                    <li>Seg</li>
-                    <li>Ter</li>
-                    <li>Qua</li>
-                    <li>Qui</li>
-                    <li>Sex</li>
-                    <li>Sab</li>
+                    <li>Sun</li>
+                    <li>Mon</li>
+                    <li>Tue</li>
+                    <li>Wed</li>
+                    <li>Thu</li>
+                    <li>Fri</li>
+                    <li>Sat</li>
                 </ul>
                 <ul className="flex flex-wrap w-full">
                     {[...Array(first_day).keys()].map((emptyDay) => (
