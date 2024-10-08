9fa233dfbdc7679c9db62756f979bbb335f32b76
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2024-10-08 02:21:33 -0300

    Update select

--- dashboard/src/components/SelectComponent.tsx
+++ dashboard/src/components/SelectComponent.tsx
@@ -1,21 +1,27 @@
 import * as React from "react";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 
-export function SelectComponent() {
+interface SelectComponentProps {
+    value: string;
+    onChange: (value: string) => void;
+    placeholder: string;
+    options: { value: string; label: string }[];
+}
+
+export function SelectComponent({ value, onChange, options, placeholder }: SelectComponentProps) {
   return (
-    <Select>
+    <Select onValueChange={onChange} value={value}>
       <SelectTrigger className="w-[180px] border-[var(--zinc)]">
         <SelectValue placeholder="Select tag" />
       </SelectTrigger>
       <SelectContent className="bg-card text-white selection:border-white">
         <SelectGroup >
-            <SelectLabel>Tags</SelectLabel>
-            <SelectItem className="hover:bg-[var(--veu)]" value="eletronics">Eletronics</SelectItem>
-            <SelectItem className="hover:bg-[var(--veu)]" value="drinks">Drinks</SelectItem>
-            <SelectItem className="hover:bg-[var(--veu)]" value="food">Food</SelectItem>
-            <SelectItem className="hover:bg-[var(--veu)]" value="clothes">Clothes</SelectItem>
-            <SelectItem className="hover:bg-[var(--veu)]" value="leisure">Leisure</SelectItem>
-            <SelectItem className="hover:bg-[var(--veu)]" value="others">Others</SelectItem>
+            <SelectLabel>{placeholder}</SelectLabel>
+                {options.map((option) => (
+                    <SelectItem key={option.value} className="hover:bg-[var(--veu)]" value={option.value}>
+                        {option.label}
+                    </SelectItem>
+                ))}
         </SelectGroup>
       </SelectContent>
     </Select>
