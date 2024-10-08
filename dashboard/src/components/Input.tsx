c06e138f11f1ce2886f5cb4d80afc0aba5a56157
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2024-10-08 02:20:30 -0300

    feat: update input component

--- dashboard/src/components/Input.tsx
+++ dashboard/src/components/Input.tsx
@@ -25,7 +25,7 @@ export default function Input({
                 type={type}
                 value={value}
                 onChange={onChange}
-                className={`w-56 h-6 px-2 text-base border-b-2 border-zinc-600 focus:outline-none bg-transparent ${className}`}
+                className={`w-min h-6 px-2 text-base border-b-2 border-zinc-800 focus:outline-none bg-transparent ${className}`}
                 {...props}
             />
         </div>
