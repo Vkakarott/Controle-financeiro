debe8c187cdd59ae6b7de0f7ebbe5fa1fb44cdc7
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2024-10-08 02:19:31 -0300

    feat: add className ultil

--- dashboard/src/components/Avatar.tsx
+++ dashboard/src/components/Avatar.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 interface AvatarProps {
     src?: string;
+    className?: string;
 }
 
-export default function Avatar({ src }: AvatarProps) {
+export default function Avatar({ src, className }: AvatarProps) {
     const defaultSrc = "/avatar.jpeg"; 
 
     return (
         <div className="flex">
             <img 
-                className="w-12 h-12 rounded-full"
+                className={`w-12 h-12 rounded-full ${className}`}
                 src={src || defaultSrc} 
                 alt={src ? "User Avatar" : "Default Avatar"} 
                 onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => e.currentTarget.src = defaultSrc}
