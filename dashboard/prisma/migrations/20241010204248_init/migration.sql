a795cb410ed988e1ec0715733512de493eac1557
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2024-11-02 21:42:45 -0300

    update bdprisma

---
+++ dashboard/prisma/migrations/20241010204248_init/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `fixedIncome` on the `User` table. The data in that column could be lost. The data in that column will be cast from `String` to `Float`.
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `password` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "emailVerified" DATETIME,
+    "image" TEXT,
+    "profession" TEXT,
+    "fixedIncome" REAL,
+    "payOff" INTEGER,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("createdAt", "email", "emailVerified", "fixedIncome", "id", "image", "name", "password", "profession", "updatedAt") SELECT "createdAt", "email", "emailVerified", "fixedIncome", "id", "image", "name", "password", "profession", "updatedAt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
